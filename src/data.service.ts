0ba372b2a022b8b913ae02c6058687f43cd0a85d
Stephens <Keagan.stephens@icloud.com>
2023-09-15 18:22:27 +0200

    fixing overview p4

--- src/data.service.ts
+++ src/data.service.ts
@@ -13,7 +13,7 @@ export class DataService {
   "OL7031272M"]
 
   bookInfo = {
-    toDisplay : true,
+    toDisplay : false,
     id: "OL7122393M",
     title: 'the deaufhnohg khgljyf yf ouyf ouyf otudjfout duoytfiythfou yfu e',
     publish_date: '80534',
